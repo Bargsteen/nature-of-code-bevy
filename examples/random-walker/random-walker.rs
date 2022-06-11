630218215e1e6092d30b4d51c522d20396b53ce5
Kasper Dissing Bargsteen <kasper@Kaspers-iMac.local>
2022-06-11 11:35:04 +0200

    Add gaussian and montecarlo options

--- examples/random-walker/random-walker.rs
+++ examples/random-walker/random-walker.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use rand::prelude::*;
 
+const GAUSSIAN_WALK: bool = true;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -14,16 +16,39 @@ struct Walker;
 
 fn startup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("black-circle.png"),
-        ..Default::default()
-    }).insert(Walker);
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: asset_server.load("black-circle.png"),
+            ..Default::default()
+        })
+        .insert(Walker);
 }
 
 fn walk(mut query: Query<&mut Transform, With<Walker>>) {
-    let speed = 5.;
-    let mut rng = rand::thread_rng();
+    let speed = montecarlo_step() * 20.;
     let mut walker_transform = query.single_mut();
-    walker_transform.translation.x += rng.gen_range(-speed..speed);
-    walker_transform.translation.y += rng.gen_range(-speed..speed);
+    let mut rng = rand::thread_rng();
+    let dx: f32;
+    let dy: f32;
+    if GAUSSIAN_WALK {
+        dx = rng.gen::<f32>() * speed - (speed / 2.);
+        dy = rng.gen::<f32>() * speed - (speed / 2.);
+    } else {
+        dx = rng.gen_range(-speed..speed);
+        dy = rng.gen_range(-speed..speed);
+    }
+    walker_transform.translation.x += dx;
+    walker_transform.translation.y += dy;
+}
+
+fn montecarlo_step() -> f32 {
+    let mut rng = rand::thread_rng();
+    loop {
+        let r1: f32 = rng.gen();
+        let probability: f32 = r1.sqrt();
+        let r2: f32 = rng.gen();
+        if r2 < probability {
+            return r1;
+        }
+    }
 }

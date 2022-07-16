87c5e284a815e39685c8a0abed8c8486998e2ce1
Kasper Dissing Bargsteen <kasper@Kaspers-iMac.local>
2022-07-16 11:56:35 +0200

    Implement vec2

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,109 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct Vec2 {
+    x: f32,
+    y: f32,
+}
+
+impl Add for Vec2 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl AddAssign for Vec2 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl Sub for Vec2 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl SubAssign for Vec2 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
+
+impl Mul<f32> for Vec2 {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+impl MulAssign<f32> for Vec2 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.x *= rhs;
+        self.y *= rhs;
+    }
+}
+
+impl Div<f32> for Vec2 {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x / rhs,
+            y: self.y / rhs,
+        }
+    }
+}
+
+impl DivAssign<f32> for Vec2 {
+    fn div_assign(&mut self, rhs: f32) {
+        self.x /= rhs;
+        self.y /= rhs;
+    }
+}
+
+impl Vec2 {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+    pub fn mag(&self) -> f32 {
+        f32::sqrt(self.x.powi(2) + self.y.powi(2))
+    }
+    pub fn normalize(&mut self) {
+        let m = self.mag();
+        *self /= m;
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn addition_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_addition() {
+        assert_eq!(
+            Vec2::new(-10., 10.) + Vec2::new(10., 10.),
+            Vec2::new(0., 20.)
+        );
+    }
+
+    #[test]
+    fn test_magnitude() {
+        assert_eq!(Vec2::new(3., 4.).mag(), 5.);
     }
 }
